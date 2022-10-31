408005cfeab78f41e0b260cb00922ea3c1298d9f
Daniel Zurawski <djzurawski@gmail.com>
2022-10-30 18:46:08 -0600

    Adding more domain flexibility

--- danwrf.py
+++ danwrf.py
@@ -1,13 +1,14 @@
 from netCDF4 import Dataset
-from datetime import datetime, timedelta
+from datetime import timedelta
 import os
 from dateutil import parser
 import numpy as np
 import multiprocessing as mp
+import argparse
 
-import basemap as bmap
+
 import matplotlib.pyplot as plt
-import plot
+
 import plot2
 
 import cartopy.crs as crs
@@ -15,11 +16,8 @@ import cartopy.crs as crs
 from wrf import (
     getvar,
     interplevel,
-    to_np,
     latlon_coords,
     get_cartopy,
-    cartopy_xlim,
-    cartopy_ylim,
 )
 
 
@@ -49,22 +47,21 @@ def coriolis_parameter(lat_degrees):
     return f
 
 
-def domain_netcdf_files(domain="d02", path=UT_NC_DIR):
-    domain_files = sorted([f for f in os.listdir(path) if domain in f])
+def domain_netcdf_files(wrf_domain="d02", path=UT_NC_DIR):
+    domain_files = sorted([f for f in os.listdir(path) if wrf_domain in f])
     return domain_files
 
 
 def accumulated_swe_plots(
-    nc_dir=UT_NC_DIR,
-    domain="d02",
+    wrfprd_dir,
+    domain_name,
+    wrf_domain="d02",
     labels=[],
-    extent=None,
-    central_longitude=-110,
 ):
 
-    for nc_file in domain_netcdf_files(path=nc_dir, domain=domain):
+    for nc_file in domain_netcdf_files(path=wrfprd_dir, wrf_domain=wrf_domain):
 
-        ds = Dataset(nc_dir + "/" + nc_file)
+        ds = Dataset(wrfprd_dir + "/" + nc_file)
 
         swe_in = ds.variables["SNOWNC"][0] * MM_TO_IN
         lons = np.array(ds.variables["XLONG"][0])
@@ -82,7 +79,7 @@ def accumulated_swe_plots(
         title = plot2.make_title_str(
             init_time, valid_time, fhour, "swe", "danwrf", "in"
         )
-        print("saving swe", domain, cycle, fhour)
+        print("saving swe", domain_name, cycle, fhour)
 
         mid_lon = np.median(lons)
         mid_lat = np.median(lats)
@@ -96,23 +93,17 @@ def accumulated_swe_plots(
 
         ax.set_title(title)
         fig.savefig(
-            f"wrf_prod/images/{cycle}z/{domain}-{cycle}z-swe-{fhour_str}.png",
+            f"wrf_prod/images/{cycle}z/{domain_name}-{cycle}z-swe-{fhour_str}.png",
             bbox_inches="tight",
         )
         plt.close(fig)
 
 
-def accumulated_precip_plots(
-    nc_dir=UT_NC_DIR,
-    domain="d02",
-    labels=[],
-    extent=None,
-    central_longitude=-110,
-):
+def accumulated_precip_plots(wrfprd_dir, domain_name, wrf_domain="d02", labels=[]):
 
-    for nc_file in domain_netcdf_files(path=nc_dir, domain=domain):
+    for nc_file in domain_netcdf_files(path=wrfprd_dir, wrf_domain=wrf_domain):
 
-        ds = Dataset(nc_dir + "/" + nc_file)
+        ds = Dataset(wrfprd_dir + "/" + nc_file)
 
         precip_in = ds.variables["RAINNC"][0] * MM_TO_IN
         lons = np.array(ds.variables["XLONG"][0])
@@ -130,7 +121,7 @@ def accumulated_precip_plots(
         title = plot2.make_title_str(
             init_time, valid_time, fhour, "precip", "danwrf", "in"
         )
-        print("saving precip", domain, cycle, fhour)
+        print("saving precip", domain_name, cycle, fhour)
 
         mid_lon = np.median(lons)
         mid_lat = np.median(lats)
@@ -150,13 +141,13 @@ def accumulated_precip_plots(
 
         ax.set_title(title)
         fig.savefig(
-            f"wrf_prod/images/{cycle}z/{domain}-{cycle}z-precip-{fhour_str}.png",
+            f"wrf_prod/images/{cycle}z/{domain_name}-{cycle}z-precip-{fhour_str}.png",
             bbox_inches="tight",
         )
         plt.close(fig)
 
 
-def vort_500_plot(nc_path, domain):
+def vort_500_plot(nc_path, domain_name):
     ds = Dataset(nc_path)
     init_time = parser.parse(ds.START_DATE.replace("_", " "))
     cycle = str(init_time.hour).zfill(2)
@@ -190,7 +181,7 @@ def vort_500_plot(nc_path, domain):
 
     projection = get_cartopy(ht_500)
 
-    print("saving vort 500", domain, cycle, fhour)
+    print("saving vort 500", domain_name, cycle, fhour)
     fig, ax = plot2.plot_500_vorticity(
         lons,
         lats,
@@ -203,24 +194,24 @@ def vort_500_plot(nc_path, domain):
     )
 
     fig.savefig(
-        f"wrf_prod/images/{cycle}z/{domain}-{cycle}z-vort500-{fhour_str}.png",
+        f"wrf_prod/images/{cycle}z/{domain_name}-{cycle}z-vort500-{fhour_str}.png",
         bbox_inches="tight",
     )
 
     plt.close(fig)
 
 
-def vort_500_plots(nc_dir=UT_NC_DIR, domain="d01"):
+def vort_500_plots(wrfprd_dir, domain_name, wrf_domain="d01"):
     nc_paths = [
-        nc_dir + "/" + nc_file
-        for nc_file in domain_netcdf_files(path=nc_dir, domain=domain)
+        wrfprd_dir + "/" + nc_file
+        for nc_file in domain_netcdf_files(path=wrfprd_dir, wrf_domain=wrf_domain)
     ]
 
     for nc_path in nc_paths:
-        vort_500_plot(nc_path, domain)
+        vort_500_plot(nc_path, domain_name)
 
 
-def rh_700_plot(nc_path, domain):
+def rh_700_plot(nc_path, domain_name):
     ds = Dataset(nc_path)
     init_time = parser.parse(ds.START_DATE.replace("_", " "))
     cycle = str(init_time.hour).zfill(2)
@@ -251,143 +242,99 @@ def rh_700_plot(nc_path, domain):
 
     projection = get_cartopy(ht_700)
 
-    print("saving rh 700", domain, cycle, fhour)
+    print("saving rh 700", domain_name, cycle, fhour)
     fig, ax = plot2.plot_700_rh(
         lons, lats, ht_700, rh_700, u_700, v_700, title=title, projection=projection
     )
 
     fig.savefig(
-        f"wrf_prod/images/{cycle}z/{domain}-{cycle}z-rh700-{fhour_str}.png",
+        f"wrf_prod/images/{cycle}z/{domain_name}-{cycle}z-rh700-{fhour_str}.png",
         bbox_inches="tight",
     )
 
     plt.close(fig)
 
 
-def rh_700_plots(nc_dir=UT_NC_DIR, domain="d01"):
+def rh_700_plots(wrfprd_dir, domain_name, wrf_domain):
 
     nc_paths = [
-        nc_dir + "/" + nc_file
-        for nc_file in domain_netcdf_files(path=nc_dir, domain=domain)
+        wrfprd_dir + "/" + nc_file
+        for nc_file in domain_netcdf_files(path=wrfprd_dir, wrf_domain=wrf_domain)
     ]
 
     for nc_path in nc_paths:
-        rh_700_plot(nc_path, domain)
+        rh_700_plot(nc_path, domain_name)
 
 
 def tst():
     d = "/home/dan/Documents/weather/wrfprd"
-    rh_700_plots(d, domain="d02")
+    rh_700_plots(d, wrf_domain="d02")
 
 
 def tst2():
     ds = "/home/dan/Documents/weather/wrfprd/d02_08"
-    # vort_500_plot(ds, domain="d02")
-    # vort_50_plot(ds, domain="d02")
+    # vort_500_plot(ds, wrf_domain="d02")
+    # vort_50_plot(ds, wrf_domain="d02")
 
 
 def tst3():
     accumulated_swe_plots(
-        nc_dir="/home/dan/Documents/weather/wrfprd/",
-        domain="d02",
+        wrfprd_dir="/home/dan/Documents/weather/wrfprd/",
+        wrf_domain="d02",
         labels=[],
         central_longitude=-106.5,
     )
 
 
-def test_swe():
-    f = "/home/dan/Documents/weather/wrfprd/d03_08"
-    domain = "d03"
-
-    ds = Dataset(f)
-
-    swe_in = ds.variables["SNOWNC"][0] * MM_TO_IN
-    lons = np.array(ds.variables["XLONG"][0])
-    lats = np.array(ds.variables["XLAT"][0])
-    u_10 = ds.variables["U10"][0]
-    v_10 = ds.variables["V10"][0]
-
-    init_time = parser.parse(ds.START_DATE.replace("_", " "))
-    cycle = str(init_time.hour).zfill(2)
-    fhour = int(ds.variables["XTIME"][0] / 60)
-    fhour_str = str(fhour).zfill(2)
-    valid_time = init_time + timedelta(hours=fhour)
-
-    title = plot2.make_title_str(init_time, valid_time, fhour, "swe", "danwrf", "in")
-    print("saving swe", domain, cycle, fhour)
-
-    mid_lon = np.median(lons)
-    mid_lat = np.median(lats)
-    projection = crs.LambertConformal(
-        central_latitude=mid_lat, central_longitude=mid_lon
-    )
-
-    fig, ax = plot2.plot_swe(
-        lons, lats, swe_in, u10=u_10, v10=v_10, labels=[], projection=projection
-    )
-
-    ax.set_title(title)
-    fig.savefig(f"wrf_prod/images/{cycle}z/{domain}-{cycle}z-swe-{fhour_str}.png")
+def error_callback(e):
+    print(e)
 
 
-if __name__ == "__main__":
+def main(wrfprd_path, domain_name, wrf_domain="d01", labels=[]):
 
-    # CO_NC_DIR = "/home/dan/Documents/weather/wrfprd/"
-
-    """
-    accumulated_swe_plots(
-        nc_dir=CO_NC_DIR,
-        domain="d03",
-        labels=bmap.CO_D2.labels,
-        central_longitude=-106.5,
-    )
-
-
-    accumulated_swe_plots(
-        nc_dir=CO_NC_DIR,
-        domain="d02",
-        labels=[],
-        central_longitude=-106.5,
-    )
-
-    accumulated_swe_plots(
-        nc_dir=CO_NC_DIR,
-        domain="d01",
-        labels=[],
-        central_longitude=-106.5,
-    )
-
-    rh_700_plots(nc_dir=CO_NC_DIR, domain="d01")
-    rh_700_plots(nc_dir=CO_NC_DIR, domain="d02")
-
-    vort_500_plots(nc_dir=CO_NC_DIR, domain="d01")
-    vort_500_plots(nc_dir=CO_NC_DIR, domain="d02")
-
-    """
     # Do it this way because mp.Pool() freezes computer when using after calling
     # accumulated_swe_plots()
     with mp.Pool() as pool:
-        res_swe_d01 = pool.apply_async(accumulated_swe_plots, (CO_NC_DIR, "d01"))
-        res_swe_d02 = pool.apply_async(
-            accumulated_swe_plots, (CO_NC_DIR, "d02", CO_LABELS)
+        pool.apply_async(
+            accumulated_swe_plots,
+            (wrfprd_path, domain_name, wrf_domain, labels),
+            error_callback=error_callback,
         )
-        # res_swe_d03 = pool.apply_async(accumulated_swe_plots, (CO_NC_DIR, "d03", CO_LABELS))
-
-        res_precip_d01 = pool.apply_async(accumulated_precip_plots, (CO_NC_DIR, "d01"))
-        res_precip_d02 = pool.apply_async(
-            accumulated_precip_plots, (CO_NC_DIR, "d02", CO_LABELS)
+        pool.apply_async(
+            accumulated_precip_plots,
+            (wrfprd_path, domain_name, wrf_domain, labels),
+            error_callback=error_callback,
         )
-        # res_precip_d03 = pool.apply_async(accumulated_precip_plots, (CO_NC_DIR, "d03", CO_LABELS))
-
-        res_rh_700_d01 = pool.apply_async(rh_700_plots, (CO_NC_DIR, "d01"))
-        res_rh_700_d02 = pool.apply_async(rh_700_plots, (CO_NC_DIR, "d02", CO_LABELS))
-        # res_rh_700_d03 = pool.apply_async(rh_700_plots, (CO_NC_DIR, "d03"))
-
-        res_vort_500_d01 = pool.apply_async(vort_500_plots, (CO_NC_DIR, "d01"))
-        res_vort_500_d02 = pool.apply_async(
-            vort_500_plots, (CO_NC_DIR, "d02", CO_LABELS)
+        pool.apply_async(
+            rh_700_plots,
+            (wrfprd_path, domain_name, wrf_domain),
+            error_callback=error_callback,
         )
-        # res_vort_500_d03 = pool.apply_async(vort_500_plots, (CO_NC_DIR, "d03"))
+        pool.apply_async(
+            vort_500_plots,
+            (wrfprd_path, domain_name, wrf_domain),
+            error_callback=error_callback,
+        )
 
         pool.close()
         pool.join()
+
+
+if __name__ == "__main__":
+    argparser = argparse.ArgumentParser(description="Danwrf plot generator")
+    argparser.add_argument("-p", "--wrfprd_path", type=str, required=True)
+    argparser.add_argument("-d", "--domain-name", type=str, required=True)
+    argparser.add_argument("-n", "--num_nests", type=int, default=1)
+
+    args = argparser.parse_args()
+
+    domain_name = args.domain_name
+    wrfprd_path = args.wrfprd_path
+    num_nests = args.num_nests
+
+    wrf_domains = ["d0" + str(i) for i in range(1, num_nests+1)]
+    domain_names = [domain_name + d for d in wrf_domains]
+
+    for domain_name, wrf_domain in zip(domain_names, wrf_domains):
+        print(wrfprd_path, domain_name, wrf_domain)
+        main(wrfprd_path, domain_name, wrf_domain)
